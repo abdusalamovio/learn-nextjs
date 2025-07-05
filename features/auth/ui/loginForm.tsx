01745a220d66287cc30c20560a5b25f69d995211
abdusalamovio <hello.iskhak@gmail.com>
2025-07-05 17:46:01 +0500

    fix: update env and forms

--- features/auth/ui/loginForm.tsx
+++ features/auth/ui/loginForm.tsx
@@ -88,7 +88,9 @@ export function LoginForm() {
 
           <div className="flex justify-center">
             <ReCAPTCHA
-              sitekey={process.env.GOOGLE_RECAPTCHA_SITE_KEY as string}
+              sitekey={
+                process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY as string
+              }
               onChange={setRecaptchaValue}
               theme={theme === "light" ? "light" : "dark"}
             />
--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
 
-  env: {
-    GOOGLE_RECAPTCHA_SITE_KEY: process.env.GOOGLE_RECAPTCHA_SITE_KEY,
-  },
-
   images: {
     remotePatterns: [
       {
